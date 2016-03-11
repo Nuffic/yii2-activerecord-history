4c3a4293003122f9d67a1f762691d8ec43aa63e8
Kaupo Juhkam <kaupo@credy.eu>
2016-03-11 15:24:18 +0200

    merge migrations into one file

--- migrations/m160310_123638_create_history.php
+++ migrations/m160310_123638_create_history.php
@@ -1,28 +1,45 @@
 <?php
 
+use nuffic\activerecord\history\extensions\DbHistoryLogger;
+use yii\base\InvalidConfigException;
 use yii\db\Migration;
 
 class m160310_123638_create_history extends Migration
 {
+    /**
+     * @throws yii\base\InvalidConfigException
+     * @return DbHistoryLogger
+     */
+    protected function getHistoryLogger()
+    {
+        $history = Yii::$app->get('arHistory');
+        if (!($history instanceof DbHistoryLogger)) {
+            throw new InvalidConfigException('You should configure "arHistory" component to use database before executing this migration.');
+        }
+        return $history;
+    }
+
     public function up()
     {
-        $this->createTable('history', [
-            'id' => $this->primaryKey(),
-            'table_name' => $this->string()->notNull(),
-            'field_id' => $this->string()->notNull(),
-            'field_name' => $this->string()->notNull(),
-            'old_value' => $this->text(),
-            'created_at' => $this->timestamp()->notNull(),
+        $this->createTable($this->historyLogger->tableName, [
+            'id'          => $this->primaryKey(),
+            'table_name'  => $this->string()->notNull(),
+            'field_id'    => $this->string()->notNull(),
+            'field_name'  => $this->string()->notNull(),
+            'old_value'   => $this->text(),
+            'event'       => $this->integer(1)->notNull(),
+            'action_uuid' => $this->string(36)->notNull(),
+            'created_at'  => $this->timestamp()->notNull(),
         ]);
 
-        $this->createIndex('in_history_table_name_field_id', 'history', ['table_name', 'field_id']);
-        $this->createIndex('in_history_table_name_field_name', 'history', ['table_name', 'field_name', 'field_id']);
+        $this->createIndex('in_history_table_name_field_id', $this->historyLogger->tableName, ['table_name', 'field_id']);
+        $this->createIndex('in_history_table_name_field_name', $this->historyLogger->tableName, ['table_name', 'field_name', 'field_id']);
     }
 
     public function down()
     {
-        $this->dropIndex('in_history_table_name_field_id', 'history');
-        $this->dropIndex('in_history_table_name_field_name', 'history');
-        $this->dropTable('history');
+        $this->dropIndex('in_history_table_name_field_id', $this->historyLogger->tableName);
+        $this->dropIndex('in_history_table_name_field_name', $this->historyLogger->tableName);
+        $this->dropTable($this->historyLogger->tableName);
     }
 }
